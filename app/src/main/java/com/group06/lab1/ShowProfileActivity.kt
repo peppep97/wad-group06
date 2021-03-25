4ef6580fd65eec78553aafce8e745d33532adc49
GIUSEPPE PIPERO s238385 <s238385@studenti.polito.it>
2021-03-25 09:06:57 +0100

    first commit

---
+++ app/src/main/java/com/group06/lab1/ShowProfileActivity.kt
@@ -0,0 +1,12 @@
+package com.group06.lab1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ShowProfileActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_show_profile)
+    }
+}
