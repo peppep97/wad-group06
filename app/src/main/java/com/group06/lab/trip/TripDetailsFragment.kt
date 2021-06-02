0cb89bc3f54a58213eb4c2bc617fc17dc8cc43c2
SMousavi90 <mousavy1990@gmail.com>
2021-06-02 17:19:16 +0200

    feat: new design for showing the trip route

--- app/src/main/java/com/group06/lab/trip/TripDetailsFragment.kt
+++ app/src/main/java/com/group06/lab/trip/TripDetailsFragment.kt
@@ -13,14 +13,13 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.RatingBar
 import android.widget.TextView
+import androidx.cardview.widget.CardView
 import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import coil.load
 import coil.request.CachePolicy
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputLayout
@@ -31,15 +30,7 @@ import com.group06.lab.MainActivity
 import com.group06.lab.R
 import com.group06.lab.extensions.toString
 import com.group06.lab.utils.Dialog
-import org.osmdroid.bonuspack.routing.OSRMRoadManager
-import org.osmdroid.bonuspack.routing.RoadManager
 import org.osmdroid.config.Configuration
-import org.osmdroid.tileprovider.tilesource.TileSourceFactory
-import org.osmdroid.util.GeoPoint
-import org.osmdroid.views.MapView
-import org.osmdroid.views.overlay.Marker
-import org.osmdroid.views.overlay.Overlay
-import org.osmdroid.views.overlay.Polyline
 import java.text.DecimalFormat
 import java.util.*
 
@@ -48,7 +39,6 @@ var tripId: String? = ""
 private var caller: String? = ""
 private var showEditButton: Boolean = false
 private lateinit var snackBar: Snackbar
-private lateinit var client: FusedLocationProviderClient
 
 class TripDetailsFragment : Fragment() {
     private lateinit var fabFav: FloatingActionButton
@@ -56,7 +46,6 @@ class TripDetailsFragment : Fragment() {
     private lateinit var btnDeleteTrip: Button
     private lateinit var btnCompleteTrip : Button
 
-    private lateinit var map : MapView;
     private val REQUEST_PERMISSIONS_REQUEST_CODE = 1;
 
     private lateinit var btnRate : Button
@@ -77,9 +66,6 @@ class TripDetailsFragment : Fragment() {
         super.onCreate(savedInstanceState)
         Configuration.getInstance().load(activity,
             PreferenceManager.getDefaultSharedPreferences(activity))
-        client = LocationServices.getFusedLocationProviderClient(requireActivity())
-        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
-        StrictMode.setThreadPolicy(policy)
     }
 
     override fun onCreateView(
@@ -132,12 +118,7 @@ class TripDetailsFragment : Fragment() {
         val imgTrip = view.findViewById<ImageView>(R.id.imgTrip)
         val tvDepTime = view.findViewById<TextView>(R.id.tvDepTime)
         val tvArrTime = view.findViewById<TextView>(R.id.tvArrTime)
-        map = view.findViewById<MapView>(R.id.map)
-        map.setTileSource(TileSourceFactory.MAPNIK)
-        map.setMultiTouchControls(true)
-        val mapController = map.controller
-        mapController.setZoom(9.5)
-        map.parent.requestDisallowInterceptTouchEvent(true)
+        val cardMap = view.findViewById<CardView>(R.id.cardMap)
 
         if(savedInstanceState != null){
 
@@ -164,11 +145,11 @@ class TripDetailsFragment : Fragment() {
             showEditButton = t.userEmail == MainActivity.mAuth.currentUser!!.email!!
 
             setHasOptionsMenu(true)
-            //fabFav.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.GONE else View.VISIBLE
-            //btnDeleteTrip.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.VISIBLE else View.GONE
-            //btnCompleteTrip.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.VISIBLE else View.GONE
-            //if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) fabFav.hide()
-            //btnShowFavoredList.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.VISIBLE else View.GONE
+            fabFav.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.GONE else View.VISIBLE
+            btnDeleteTrip.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.VISIBLE else View.GONE
+            btnCompleteTrip.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.VISIBLE else View.GONE
+            if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) fabFav.hide()
+            btnShowFavoredList.visibility = if (t.userEmail == MainActivity.mAuth.currentUser!!.email!!) View.VISIBLE else View.GONE
             fabFav.visibility = if (showFAB) View.VISIBLE else  View.VISIBLE
             if (!showFAB) fabFav.hide()
 
@@ -199,76 +180,15 @@ class TripDetailsFragment : Fragment() {
             tvDepartureDate.text = t.departureDate.toString("MMMM - dd")
             tvAvailableSeats.text = t.availableSeats.toString()
 
-
-            val origin = GeoPoint(t.depPosition.latitude, t.depPosition.longitude)
-            val destination = GeoPoint(t.arrPosition.latitude, t.arrPosition.longitude)
-
-            if (origin.latitude != 0.0 && origin.longitude != 0.0 &&
-                destination.latitude != 0.0 && destination.longitude != 0.0) {
-
-                val gcd = Geocoder(context, Locale.getDefault())
-                var addresses: List<Address> =
-                    gcd.getFromLocation(origin.latitude, origin.longitude, 1)
-
-                val startMarker = Marker(map)
-                startMarker.position = GeoPoint(origin.latitude, origin.longitude)
-                startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                if (addresses.isNotEmpty())
-                    startMarker.title = "${addresses[0].locality} - ${addresses[0].countryName}\n${addresses[0].thoroughfare ?: ""} ${addresses[0].subThoroughfare ?: ""}"
-                else
-                    startMarker.title = "Departure"
-                startMarker.id = "dep"
-
-                map.overlays?.add(startMarker)
-
-                val arrAddresses: List<Address> = gcd.getFromLocation(destination.latitude, destination.longitude, 1)
-                val endMarker = Marker(map)
-                endMarker.position = GeoPoint(destination.latitude, destination.longitude)
-                endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
-                if (arrAddresses.isNotEmpty())
-                    endMarker.title = "${arrAddresses[0].locality} - ${arrAddresses[0].countryName}\n${arrAddresses[0].thoroughfare ?: ""} ${arrAddresses[0].subThoroughfare ?: ""}"
-                else
-                    endMarker.title = "Arrival"
-                endMarker.id = "arr"
-
-                map.overlays?.add(startMarker)
-                map.overlays?.add(endMarker)
-
-                mapController.setCenter(origin)
-
-                val roadManager: RoadManager =
-                    OSRMRoadManager(requireContext(), "OBP_Tuto/1.0")
-
-                map.overlays
-                    .forEach { o -> if (o is Polyline) map.overlays.remove(o as Overlay) }
-
-                val wayPoints = ArrayList<GeoPoint>()
-                wayPoints.add(origin)
-                wayPoints.add(destination)
-                val road = roadManager.getRoad(wayPoints)
-                val roadOverlay = RoadManager.buildRoadOverlay(road)
-                roadOverlay.id = "path"
-                map.overlays.add(roadOverlay);
-                map.invalidate();
-            } else {
-                if (ActivityCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.ACCESS_FINE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                        requireContext(),
-                        Manifest.permission.ACCESS_COARSE_LOCATION
-                    ) != PackageManager.PERMISSION_GRANTED
-                ) {
-                    //
-                } else {
-                    client.lastLocation.addOnSuccessListener(
-                        requireActivity()
-                    ) { location ->
-                        run {
-                            mapController.setCenter(GeoPoint(location.latitude, location.longitude))
-                        }
-                    }
-                }
+            cardMap.setOnClickListener {
+                findNavController().navigate(
+                    R.id.action_trip_details_to_tripRouteFragment,
+                    Bundle().apply {
+                        putDouble("depLat", t.depPosition.latitude)
+                        putDouble("depLon", t.depPosition.longitude)
+                        putDouble("arrLat", t.arrPosition.latitude)
+                        putDouble("arrLon", t.arrPosition.longitude)
+                    })
             }
 
             tvDepTime.text = t.departureDate.toString("HH:mm")
@@ -476,20 +396,6 @@ class TripDetailsFragment : Fragment() {
 
 
 
-    }
-
-    override fun onResume() {
-        super.onResume();
-        Configuration.getInstance().load(activity,
-            PreferenceManager.getDefaultSharedPreferences(activity))
-        map.onResume();
-    }
-
-    override fun onPause() {
-        super.onPause();
-        Configuration.getInstance().load(activity,
-            PreferenceManager.getDefaultSharedPreferences(activity))
-        map.onPause();
     }
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
---
+++ app/src/main/java/com/group06/lab/trip/TripRouteFragment.kt
@@ -0,0 +1,190 @@
+package com.group06.lab.trip
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.location.Address
+import android.location.Geocoder
+import android.os.Bundle
+import android.os.StrictMode
+import android.preference.PreferenceManager
+import android.view.*
+import androidx.core.app.ActivityCompat
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.group06.lab.R
+import org.osmdroid.api.IGeoPoint
+import org.osmdroid.bonuspack.routing.OSRMRoadManager
+import org.osmdroid.bonuspack.routing.RoadManager
+import org.osmdroid.config.Configuration
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.MapView
+import org.osmdroid.views.overlay.Marker
+import org.osmdroid.views.overlay.Overlay
+import org.osmdroid.views.overlay.Polyline
+import java.util.*
+
+private var isDeparture: Boolean? = true
+private var edit: Boolean? = false
+private var index = -1
+private var trip_id: String? = ""
+private var depLat: Double? = 0.0
+private var arrLat: Double? = 0.0
+private var depLon: Double? = 0.0
+private var arrLon: Double? = 0.0
+
+class TripRouteFragment : Fragment() {
+    private lateinit var map: MapView;
+    private lateinit var client: FusedLocationProviderClient
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Configuration.getInstance().load(
+            activity,
+            PreferenceManager.getDefaultSharedPreferences(activity)
+        )
+        client = LocationServices.getFusedLocationProviderClient(requireActivity())
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_trip_route, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        map = view.findViewById<MapView>(R.id.mapRoute)
+        map.setTileSource(TileSourceFactory.MAPNIK);
+        map.setMultiTouchControls(true)
+        val mapController = map.controller
+        mapController.setZoom(13)
+        if (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
+            return
+        }
+        client.lastLocation.addOnSuccessListener(
+            requireActivity()
+        ) { location ->
+            run {
+                depLat = arguments?.getDouble("depLat")
+                depLon = arguments?.getDouble("depLon")
+                arrLat = arguments?.getDouble("arrLat")
+                arrLon = arguments?.getDouble("arrLon")
+
+                if (depLat != 0.0 && depLon != 0.0 &&
+                    arrLat != 0.0 && arrLon != 0.0
+                ) {
+                    val origin = GeoPoint(depLat!!, depLon!!)
+                    val destination = GeoPoint(arrLat!!, arrLon!!)
+
+                    if (origin.latitude != 0.0 && origin.longitude != 0.0 &&
+                        destination.latitude != 0.0 && destination.longitude != 0.0
+                    ) {
+
+                        val gcd = Geocoder(context, Locale.getDefault())
+                        val addresses: List<Address> =
+                            gcd.getFromLocation(origin.latitude, origin.longitude, 1)
+
+                        val startMarker = Marker(map)
+                        startMarker.position = GeoPoint(origin.latitude, origin.longitude)
+                        startMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                        if (addresses.isNotEmpty())
+                            startMarker.title =
+                                "${addresses[0].locality} - ${addresses[0].countryName}\n${addresses[0].thoroughfare ?: ""} ${addresses[0].subThoroughfare ?: ""}"
+                        else
+                            startMarker.title = "Departure"
+                        startMarker.id = "dep"
+
+                        map.overlays?.add(startMarker)
+
+                        val arrAddresses: List<Address> =
+                            gcd.getFromLocation(destination.latitude, destination.longitude, 1)
+                        val endMarker = Marker(map)
+                        endMarker.position = GeoPoint(destination.latitude, destination.longitude)
+                        endMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM)
+                        if (arrAddresses.isNotEmpty())
+                            endMarker.title =
+                                "${arrAddresses[0].locality} - ${arrAddresses[0].countryName}\n${arrAddresses[0].thoroughfare ?: ""} ${arrAddresses[0].subThoroughfare ?: ""}"
+                        else
+                            endMarker.title = "Arrival"
+                        endMarker.id = "arr"
+
+                        map.overlays?.add(startMarker)
+                        map.overlays?.add(endMarker)
+
+                        mapController.setCenter(origin)
+
+                        val roadManager: RoadManager =
+                            OSRMRoadManager(requireContext(), "OBP_Tuto/1.0")
+
+                        map.overlays
+                            .forEach { o -> if (o is Polyline) map.overlays.remove(o as Overlay) }
+
+                        val wayPoints = ArrayList<GeoPoint>()
+                        wayPoints.add(origin)
+                        wayPoints.add(destination)
+                        val road = roadManager.getRoad(wayPoints)
+                        val roadOverlay = RoadManager.buildRoadOverlay(road)
+                        roadOverlay.id = "path"
+                        map.overlays.add(roadOverlay);
+                        map.invalidate();
+                    } else {
+                        if (ActivityCompat.checkSelfPermission(
+                                requireContext(),
+                                Manifest.permission.ACCESS_FINE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                                requireContext(),
+                                Manifest.permission.ACCESS_COARSE_LOCATION
+                            ) != PackageManager.PERMISSION_GRANTED
+                        ) {
+                            //
+                        } else {
+                            client.lastLocation.addOnSuccessListener(
+                                requireActivity()
+                            ) { location ->
+                                run {
+                                    mapController.setCenter(
+                                        GeoPoint(
+                                            location.latitude,
+                                            location.longitude
+                                        )
+                                    )
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume();
+        Configuration.getInstance().load(
+            activity,
+            PreferenceManager.getDefaultSharedPreferences(activity)
+        )
+        map.onResume();
+    }
+
+    override fun onPause() {
+        super.onPause();
+        Configuration.getInstance().load(
+            activity,
+            PreferenceManager.getDefaultSharedPreferences(activity)
+        )
+        map.onPause();
+    }
+}
